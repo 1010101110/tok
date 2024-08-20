3ec645a4e5a097dec6ea2fcb2937a51939d5dd78
lilith hiatt <lilith.hiatt@us.abb.com>
2024-08-20 11:49:55 -0500

    combined resource page, filtering logic

--- db.sql
+++ db.sql
@@ -1,26 +1,28 @@
 CREATE TABLE art (
-   art_id serial PRIMARY KEY,
+   id serial PRIMARY KEY,
    title varchar(280) NULL,
    author varchar(280) NULL,
 
    summary varchar(280) NULL,
    markdown TEXT NULL,
 
-   created TIMESTAMP DEFAULT NOW()
+   created TIMESTAMP DEFAULT NOW(),
+   edited TIMESTAMP DEFAULT NOW()
 );
 
 CREATE TABLE res (
-   res_id serial PRIMARY KEY,
+   id serial PRIMARY KEY,
    res_type varchar(50) NULL,
    res_region varchar(5) NULL,
 
    title varchar(280) NULL,
-   summary varchar(280) NULL,
    markdown TEXT NULL,
 
    street_address TEXT NULL,
-   phone varchar(20) NULL,
+   phone varchar(50) NULL,
+   email varchar(200) NULL,
    website varchar(280) NULL,
 
-   created TIMESTAMP DEFAULT NOW()
+   created TIMESTAMP DEFAULT NOW(),
+   edited TIMESTAMP DEFAULT NOW()
 );
--- server.js
+++ server.js
@@ -2,13 +2,20 @@
 const path = require('path')
 const fs = require('fs')
 
-const fastify = require('fastify')({
-  logger: true,
-  https:{
-    key:fs.readFileSync(path.resolve('/etc/letsencrypt/live/transgenderok.com/privkey.pem')),
-    cert:fs.readFileSync(path.resolve('/etc/letsencrypt/live/transgenderok.com/fullchain.pem')),
-  }
-})
+const debug = true
+
+var fastify
+if(debug){
+ fastify = require('fastify')();
+}else{
+  fastify = require('fastify')({
+    logger: true,
+    https:{
+      key:fs.readFileSync(path.resolve('/etc/letsencrypt/live/transgenderok.com/privkey.pem')),
+      cert:fs.readFileSync(path.resolve('/etc/letsencrypt/live/transgenderok.com/fullchain.pem')),
+    }
+  });
+}
 
 const Pool = require('pg').Pool
 
@@ -29,29 +36,53 @@ fastify.get('/', (request, reply) => {
   reply.sendFile('index.html')
 })
 
+fastify.get('/resources', (request, reply) => {
+  reply.sendFile('resources.html')
+})
+
+fastify.get('/articles', (request, reply) => {
+  reply.sendFile('articles.html')
+})
+
+//api
 fastify.get('/api/res/all', async (request, reply) => {
-  return await dbpool.query`select * from res`.rows
+  //return await dbpool.query`select * from res`.rows
+
+  return [
+    {id:1,title:'asdfasdfasdfasdf 111',res_type:'medical'},
+    {id:2,title:'uuuuuuuuu2 22  2',res_type:'place'},
+  ]
 })
 
 fastify.get('/api/art/all', async (request, reply) => {
   return await dbpool.query`select * from art`.rows
 })
 
-//https server
-fastify.listen({ port: 443, host: '0.0.0.0' }, (err, address) => {
-  if (err) throw err
-})
+
+//////////////////SERVERS
+
+if(debug){
+  fastify.listen({port:3333, host: '0.0.0.0'}, (err, address) => {
+    if (err) throw err
+  })
+}else{
+
+  //https server
+  fastify.listen({ port: 443, host: '0.0.0.0' }, (err, address) => {
+    if (err) throw err
+  })
 
 // http redirect
-const fastify2 = require('fastify')({ logger: false })
-fastify2.get('*', (request, reply) => {
-  reply.redirect('https://transgenderok.com')
-})
-fastify2.listen({port:80, host:'0.0.0.0'}, async (err, address) => {
-  //err log
-  if (err) throw err
+  const fastify2 = require('fastify')({ logger: false })
+  fastify2.get('*', (request, reply) => {
+    reply.redirect('https://transgenderok.com')
+  })
+  fastify2.listen({port:80, host:'0.0.0.0'}, async (err, address) => {
+    //err log
+    if (err) throw err
 
-  //init db
-  const dbresp = await dbpool.query('SELECT NOW()')
-  console.log('db init ' + dbresp.rows[0].now)
-})
+    //init db
+    const dbresp = await dbpool.query('SELECT NOW()')
+    console.log('db init ' + dbresp.rows[0].now)
+  })
+}
